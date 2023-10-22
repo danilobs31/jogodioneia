fc921f80f7d64fae965de101fed0185c2278dbde
Gustavo Borges <gustavobpl21@gmail.com>
2023-10-22 14:01:51 -0300

    Adicionado menu, imagens, e botoes interativos

--- cenas/Menu.gd
+++ cenas/Menu.gd
@@ -9,9 +9,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
 
 func _on_jogar_pressed():
 	get_tree().change_scene_to_file("res://cenas/mundo.tscn")
+	# Apertar jogar muda para a cena "mundo"
+	pass
 
 func _on_sair_pressed():
 	get_tree().quit() 
+	#sai do jogo
+
+
+
+func _on_tutorial_pressed():
+	get_tree().change_scene_to_file("res://cenas/tutorial.tscn")
